cd3a5a5b91306f0afdce790b31032a710e5d4411
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-27 03:40:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -63,7 +63,7 @@ class Song
     song_array[1] = song_array[1].chomp(".mp3")
     song = self.new
     song.name = name[1]
-    song.artist_name = artist_name[0]
+    song.artist_name = name[0]
     song
   end
   # def self.new_from_filename(filename)
